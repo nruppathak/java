a32df2dc59cfd99d92779ef34603a2755bb6f67d
NRUP PATHAK <nruppathak@gmail.com>
2025-08-21 10:46:55 +0530

    temperaturecheck

---
+++ temperaturecheck.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class temperaturecheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the temperature: ");
+        int temperature = sc.nextInt() ; 
+
+        if (temperature > 0) {
+            System.out.println("Safe for outside activities");
+        } else if (temperature <= 0 ) {
+            System.out.println("Too cold for output activities");
+        }
+        }
+    }
+
