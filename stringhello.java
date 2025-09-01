90b28b727cb9f1a9017596ad3c6a68f8bef7e159
NRUP PATHAK <nruppathak@gmail.com>
2025-09-01 08:59:49 +0530

    helloworld

---
+++ stringhello.java
@@ -0,0 +1,7 @@
+public class stringhello {
+    public static void main(String[] args) {
+        String str = "Hello";
+        String concat = str +" World";
+        System.out.println(concat);
+    }
+}
