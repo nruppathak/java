2485f567e5defa64ded4ab5d255d14c7bafcc327
NRUP PATHAK <nruppathak@gmail.com>
2025-09-04 09:29:41 +0530

    practicesheet2

---
+++ practicesheet2.java
@@ -0,0 +1,14 @@
+*public class practicesheet {
+    public static void myFunc() {
+        int x = 5;
+        int y = 10;
+        int c = x+y;
+        System.out.println(c);
+    }
+    public static void main(String[] args) {
+        practicesheet obj1 = new practicesheet();
+        System.out.println("Method Calling");
+        
+        myFunc();
+    }
+}  
